ed851d7a72adb33a4535d4e4b33ed774173110ae
amirhoseinsazizavareh <158080807+amirhoseinsazizavareh@users.noreply.github.com>
2024-01-29 20:50:38 +0330

    Add files via upload
    امیرحسین سازی زواره

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[100];
+
+        
+        Random random = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 1000);
+        }
+
+      
+        int smallest = numbers[0];
+        int largest = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        Console.WriteLine("small: " + smallest);
+        Console.WriteLine("big: " + largest);
+    }
+}
